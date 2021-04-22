0ae2e00eb78454de10b16722cff4abffeabf4eb6
James Kinley <72576466+jrkinley@users.noreply.github.com>
2021-04-22 15:18:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.2.0",
     author="James Kinley",
     author_email="jamesrobertkinley@gmail.com",
-	license="MIT",
+    license="MIT",
     description="API wrapper for APIv4 of Pardot marketing automation software.",
     keywords="pardot",
     url="https://github.com/jrkinley/PyPardot4",
